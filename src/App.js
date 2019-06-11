f166e7dc5f0d74f76892aaae8eb147545093bd4a
Raffi <fikash@gmail.com>
2019-06-11 10:18:27 -0700

    added basic redux logic

--- src/App.js
+++ src/App.js
@@ -1,13 +1,44 @@
 import React from "react";
 import "./App.css";
+import Counter from "./Counter"
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+const initialState = {
+  count: 42
+}
+
+function reducer(state = initialState, action) {
+  switch (action.type) {
+    case "INCREMENT":
+      return {
+        count: state.count + 1
+      }
+
+    case "DECREMENT":
+      return {
+        count: state.count - 1
+      }
+
+    default:
+      return state;
+  }
+}
+
+
+const store = createStore(reducer);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header" />
       <h1>Character Generator App</h1>
+      <Provider store={store}>
+        <Counter />
+      </Provider>
     </div>
   );
 }
+
 
 export default App;
